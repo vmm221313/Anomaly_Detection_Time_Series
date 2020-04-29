c606814734f03df4f92b18f3789a7c55ac0909ed
vmm221313 <varun.m.madhavan221313@gmail.com>
2020-04-29 20:22:14 +0530

    Update DataAnalysis and add papers

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class training_args():
     def __init__(self):
         self.batch_size = 16
         self.num_epochs = 100
-        self.weight_decay = 0.0001
+        self.weight_decay = 0.01
         self.learning_rate = 0.01
         self.device = torch.device('cuda:0' if torch.cuda. is_available() else 'cpu')
         
@@ -26,7 +26,7 @@ class vanillaWaveNet_args(training_args):
     def __init__(self):
         super().__init__()
         self.val_seq_len = 7
-        self.learning_rate = 0.1
+        self.learning_rate = 0.01
         self.train_seq_len = 30 # if you change this also change the num_dcconv_layers (see formula)
         self.conv_kernel_size = 4
         self.conv_out_channels = 32
