551b474a315494860ef786a289c3796c0259fcca
Varun Madhavan <VarunMadhavan@MM-MBPTB.local>
2020-05-01 17:12:01 +0530

    Cleanup

--- .ipynb_checkpoints/config-checkpoint.py
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -9,7 +9,7 @@ class training_args():
         self.weight_decay = 0.01
         self.learning_rate = 0.01
         self.device = torch.device('cuda:0' if torch.cuda. is_available() else 'cpu')
-        
+
 class CNN_args(training_args):
     def __init__(self):
         super().__init__()
@@ -18,10 +18,10 @@ class CNN_args(training_args):
         self.conv_kernel_size = 4
         self.conv_out_channels = 8
         self.max_pool_kernel_size = 2
-        self.train_dl = DataLoader(dataset('data/energy_daily_train.csv', self), batch_size=self.batch_size)
-        self.val_dl = DataLoader(dataset('data/energy_daily_val.csv', self), batch_size=self.batch_size)
-        self.test_dl = DataLoader(dataset('data/energy_daily_test.csv', self), batch_size=1)
-        
+        self.train_dl = DataLoader(dataset('data/2017_energy_5_min_train.csv', self), batch_size=self.batch_size)
+        self.val_dl = DataLoader(dataset('data/2017_energy_5_min_val.csv', self), batch_size=self.batch_size)
+        self.test_dl = DataLoader(dataset('data/2017_energy_5_min_test.csv', self), batch_size=1)
+
 class vanillaWaveNet_args(training_args):
     def __init__(self):
         super().__init__()
@@ -31,10 +31,10 @@ class vanillaWaveNet_args(training_args):
         self.conv_kernel_size = 4
         self.conv_out_channels = 32
         self.dilation_rates = [2**i for i in range(4)] #receptive_field = kernel_size*2**(num_dcconv_layers-1)
-        self.train_dl = DataLoader(dataset('data/energy_daily_train.csv', self), batch_size=self.batch_size)
-        self.val_dl = DataLoader(dataset('data/energy_daily_val.csv', self), batch_size=self.batch_size)
-        self.test_dl = DataLoader(dataset('data/energy_daily_test.csv', self), batch_size=1)
-        
+        self.train_dl = DataLoader(dataset('data/2017_energy_5_min_train.csv', self), batch_size=self.batch_size)
+        self.val_dl = DataLoader(dataset('data/2017_energy_5_min_val.csv', self), batch_size=self.batch_size)
+        self.test_dl = DataLoader(dataset('data/2017_energy_5_min_test.csv', self), batch_size=1)
+
 class LSTM_args(training_args):
     def __init__(self):
         super().__init__()
@@ -50,9 +50,9 @@ class LSTM_args(training_args):
         
 
 
-        
 
-    
+
+
     
 
     
